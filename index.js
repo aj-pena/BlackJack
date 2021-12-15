5b87085691b7cd0affa44f9e06d1788476f63926
Andres Pena <penchjpc@gmail.com>
2021-12-15 09:18:34 -0500

    defined player object and renderGame() function

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-
+let player = {
+    name: "Andres",
+    chips: 500
+}
 
 let cards = []
 let sum = 0
@@ -9,3 +12,25 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
